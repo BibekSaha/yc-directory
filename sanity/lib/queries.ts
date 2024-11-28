b08277c4cb928150b67136eca61f38382f96851a
Bibek Saha <thebibeksaha@gmail.com>
2024-11-28 08:42:48 +0530

    Started the partial pre-rendering of startup page

--- sanity/lib/queries.ts
+++ sanity/lib/queries.ts
@@ -13,3 +13,16 @@ export const STARTUPS_QUERY =
     _id, name, image, bio
   }
 }`);
+
+export const STARTUP_BY_ID_QUERY =
+  defineQuery(`*[_type == "startup" && _id == $id][0] {
+  _id,
+  title, 
+  slug,
+  _createdAt, 
+  views,
+  pitch,
+  author -> {
+    _id, name, image, bio
+  }
+}`);
