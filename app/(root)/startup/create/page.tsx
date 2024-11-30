6a8ea6c1853db2ef95cbb7c076ede52647d4014e
Bibek Saha <thebibeksaha@gmail.com>
2024-11-30 13:27:44 +0530

    Created the onboarding of startup

--- app/(root)/startup/create/page.tsx
+++ app/(root)/startup/create/page.tsx
@@ -1,9 +1,16 @@
-export default function CreateStartupPage() {
+import { auth } from '@/auth';
+import StartupForm from '@/components/StartupForm';
+import { redirect } from 'next/navigation';
+
+export default async function CreateStartupPage() {
+  const session = await auth();
+  if (!session) redirect('/');
   return (
     <>
       <section className="pink_container !min-h-[230px]">
         <h1 className="heading">Submit Your Startup Pitch</h1>
       </section>
+      <StartupForm />
     </>
   );
 }
