a84c5800ed48bb41b7d692887ba4fe1445153449
Bibek Saha <thebibeksaha@gmail.com>
2024-12-02 09:54:20 +0530

    Ignored eslint errors only during build

--- next.config.ts
+++ next.config.ts
@@ -27,6 +27,11 @@ const nextConfig: NextConfig = {
     // !! WARN !!
     ignoreBuildErrors: true,
   },
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
   /* config options here */
 };
 
